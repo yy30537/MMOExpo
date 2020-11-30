61d8e9c009e86228c5d843968c57e2a16dffd757
Yang Yang <46108589+yy30537@users.noreply.github.com>
2020-11-29 18:52:33 -0800

    First commit
    Added Unity project to Github

---
+++ Expo/Assets/Scripts/Chat_Manager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat_Manager : MonoBehaviour
+{
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+    public int maxMessages = 25;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat(chatBox.text);
+                chatBox.text = "";
+            }
+        }
+        else
+        {
+            if (!(chatBox.isFocused) && Input.GetKeyDown(KeyCode.Return))
+            {
+                chatBox.ActivateInputField();
+            }
+        }
+    }
+
+    public void SendMessageToChat(string text)
+    {
+        if (messageList.Count >= maxMessages)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+        newMessage.text = text;
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+        newMessage.textObject = newText.GetComponent<Text>();
+        newMessage.textObject.text = newMessage.text;
+        messageList.Add(newMessage);
+    }
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+}
