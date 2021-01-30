c5cc45aae9e6575abeb069455a0f4c9fbca36ea3
Yang Yang <46108589+yy30537@users.noreply.github.com>
2021-01-29 22:50:06 -0800

    Created and connected to a photon engine app, and created some canvas
     added two canvas for loading screen and three buttons for utility (creating avatar, registration and quit game), the functionalities are to be implemented

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -8,19 +8,20 @@ public class Launcher : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Connecting to Master Server ...");
+        Debug.Log("Connecting to MMO Expo ...");
         PhotonNetwork.ConnectUsingSettings();
     }
 
     // a callback called by Photon when we successfully connect to the master server
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected to Master Server");
+        Debug.Log("Connected to MMO Expo");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
+        MenuManager.instance.OpenMenu("title");
         Debug.Log("Joined Lobby");
     }
 
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,15 +6,41 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] Menu[] menus;
 
+    public static MenuManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public void OpenMenu(string menuName)
     {
-
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                OpenMenu(menus[i]);
+            } else if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
     }
 
     public void OpenMenu(Menu menu)
     {
-
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
     }
 
-
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
 }
