f8b15c15160a83f2cf5c61aa2015e13fe597d3e9
babikoffb <35248670+babikoffb@users.noreply.github.com>
2021-01-31 19:01:23 -0800

    Two users in the same server

---
+++ Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        Debug.Log("Player Created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
