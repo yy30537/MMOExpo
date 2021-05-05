a9f0d0f6ea6e512b78fc53d9930e2ecd57b4129c
Yang Yang <46108589+yy30537@users.noreply.github.com>
2021-05-04 23:00:38 -0700

    new auth ui + install playfab

---
+++ Assets/Scripts/ChangeInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class ChangeInput : MonoBehaviour
+{
+
+    EventSystem system;
+    public Selectable firstInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
+        {
+            // to enable tab to move to next selectable object
+            Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if (previous != null)
+            {
+                previous.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            // to enable tab to move to next selectable object
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if (next != null)
+            {
+                next.Select();
+            }
+        }
+    }
+}
+
